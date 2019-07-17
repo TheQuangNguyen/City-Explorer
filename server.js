3a7764332e5ea752fb4c69651b62ff020fc7707d
Quang Nguyen <quangn2595@gmail.com>
2019-07-17 09:43:06 -0700

    got maps and darksky and eventbrite to work

--- server.js
+++ server.js
@@ -20,17 +20,32 @@ function Location(query, res) {
 
 function Weather(day) { 
   this.forecast = day.summary;
-  this.time = new Date(day.time * 1000).toDateString;
+  this.time = new Date(day.time * 1000).toDateString();
+}
+
+function Event(place) { 
+  this.link = place.url;
+  this.name = place.name.text;
+  this.event_date = new Date(place.start.local).toDateString();
+  this.summary = place.summary;
+}
+
+function Movie() {
+  this.title
+  this.overview
+  this.average_votes
+  this.total_votes
+  this.image_url
+  this.popularity
+  this.released_on
 }
 
 function searchToLatLong(req, res) { 
   const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.data}&key=${process.env.GEOCODE_API_KEY}`;
-
-  console.log(geocodeUrl);
+  let location;
   return superagent.get(geocodeUrl)
-    .then(res => { 
-      const location = new Location(req.query.data, JSON.parse(res.text));
-      console.log(location);
+    .then(data => { 
+      location = new Location(req.query.data, JSON.parse(data.text));
       res.send(location);
     })
     .catch(err => { 
@@ -42,12 +57,11 @@ function getWeather(req, res) {
   const darkskyUrl =  `https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${req.query.data.latitude},${req.query.data.longitude}`;
 
   return superagent.get(darkskyUrl)
-    .then(res => {
-      const weatherEntries = res.body.daily.data.map(day => {
+    .then(data => {
+      const weatherEntries = data.body.daily.data.map(day => {
         return new Weather(day)
       })
-
-      req.send(weatherEntries);
+      res.send(weatherEntries);
     })
     .catch(err => {
       res.send(err);
@@ -55,26 +69,38 @@ function getWeather(req, res) {
 }
 
 function getMovies(req, res) {
-  const moviedbUrl = ``
+  // const moviedbUrl = ``
 }
 
 function getYelp(req, res) { 
-
+  const darkskyUrl =  `https://api.yelp.com/v3/businesses/search?`;
 }
 
 function getHiking(req, res) { 
-
+  
 }
 
 function getEvents(req, res) { 
-  
+  const eventbriteUrl = `https://www.eventbriteapi.com/v3/events/search?location.longitude=${req.query.data.longitude}&location.latitude=${req.query.data.latitude}&token=${process.env.EVENTBRITE_API_KEY}`;
+
+  return superagent.get(eventbriteUrl)
+    .then(data => {
+      const eventsNearby = [];
+      for (let i = 0; i < 20; i++) {
+        eventsNearby.push(new Event(data.body.events[i]));
+      }
+      res.send(eventsNearby);
+    })
+    .catch(err => {
+      res.send(err);
+    })
 }
 
 app.get('/location', searchToLatLong);
 app.get('/weather', getWeather);
-app.get('./movies', getMovies);
-app.get('./yelp', getYelp);
-app.get('./trails', getHiking);
-app.get('./events', getEvents);
+app.get('/movies', getMovies);
+app.get('/yelp', getYelp);
+app.get('/trails', getHiking);
+app.get('/events', getEvents);
 
 app.listen(PORT, () => console.log(`App is up on ${PORT}`));
